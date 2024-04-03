567debd2aeb96779a78cf08043660ce77cf5be59
jessun <jessunz@outlook.com>
2024-04-03 17:18:55 +0800

    feat(nvim): add plugin "nvim-treesitter"

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -69,6 +69,23 @@ require("lazy").setup({
     { 'fannheyward/telescope-coc.nvim' },
 
     -- ========================================================================
+    -- https://github.com/nvim-treesitter/nvim-treesitter code highlight
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    },
+
+    -- ========================================================================
     -- https://github.com/folke/todo-comments.nvim 注释关键字高亮
     {
         "folke/todo-comments.nvim",
