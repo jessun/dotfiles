833650417b6a2b9b5f14d2e6c955a0a6183697a7
jessun <jessun.zhang@outlook.com>
2024-08-19 15:17:00 +0000

    fix(chezmoi): fix typo

--- _scripts/manjaro_init.sh
+++ _scripts/manjaro_init.sh
@@ -6,7 +6,7 @@ yay --noconfirm
 
 yay -S \
     base-devel socat bison\
-    fish fishe tmux fzf \
+    fish fisher tmux fzf \
     git \
     ripgrep gitui rust-analyzer fd \
     neovim-nightly-bin python-neovim \
