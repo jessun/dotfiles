562fac25d4e95ca0cef7f7600e67aaeaf896851f
jessun <jessun.zhang@outlook.com>
2024-08-22 17:05:58 +0000

    fix(chezmoi): fix scripts

--- _scripts/manjaro_init.sh
+++ _scripts/manjaro_init.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 sudo pacman-mirrors -m rank -c China
+sudo pacman -S base-devel socat bison
 
-yay --noconfirm
+sudo yay --noconfirm
 
 
-yay -S \
-    base-devel socat bison\
-    fish fisher tmux fzf \
-    git \
-    ripgrep gitui rust-analyzer fd \
+
+sudo yay -S  --noconfirm \
+    fish fisher tmux fzf git \
+    rustup ripgrep gitui rust-analyzer fd \
     neovim-nightly-bin python-neovim \
     pnpm yarn npm \
     tmux-plugin-manager \
-    rustup \
-    rust-analyzer \
-    go \
-    --noconfirm
+    go
