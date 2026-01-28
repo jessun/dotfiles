0d41c5cd264084fd74f544c0a32b36e46de81be0
jessun <jessun.pro@gmail.com>
2026-01-28 20:48:13 +0800

    feat(plugins): add noice and monokai plugins
    Enable coc.nvim by default and disable wilder.nvim.
    Add go.nvim (disabled) and refactor noice.nvim placement.

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -2,7 +2,37 @@ local cmp = require("cmp")
 local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 
-require("luasnip.loaders.from_vscode").lazy_load()
+local set_hl = vim.api.nvim_set_hl
+
+-- CmpNormal: 补全菜单的背景
+set_hl(0, "CmpNormal", { bg = "#4C566A", fg = "#D8DEE9" })
+-- CmpBorder: 边框颜色 (fg 使用 Nord 蓝 #81A1C1，bg 必须和上面一样！关键！)
+set_hl(0, "CmpBorder", { fg = "#81A1C1", bg = "#2E3440" })
+-- CmpSel: 被选中的条目 (bg 使用 Nord 高亮灰 #434C5E，fg 使用白色)
+set_hl(0, "CmpSel", { bg = "#D08770", fg = "#ECEFF4", bold = true })
+-- (可选) 文档悬浮窗的颜色，可以稍微浅一点区分
+set_hl(0, "CmpDoc", { bg = "#3B4252", fg = "#D8DEE9" })
+set_hl(0, "CmpDocBorder", { fg = "#81A1C1", bg = "#2E3440" })
+-- 灰白色系：文本、变量
+set_hl(0, "CmpItemKindVariable", { fg = "#D8DEE9" })
+set_hl(0, "CmpItemKindText", { fg = "#D8DEE9" })
+
+-- 蓝色系：函数、方法
+set_hl(0, "CmpItemKindFunction", { fg = "#88C0D0" })
+set_hl(0, "CmpItemKindMethod", { fg = "#88C0D0" })
+
+-- 绿色系：字符串、类
+set_hl(0, "CmpItemKindKeyword", { fg = "#81A1C1" })
+set_hl(0, "CmpItemKindProperty", { fg = "#81A1C1" })
+set_hl(0, "CmpItemKindUnit", { fg = "#81A1C1" })
+
+-- 黄橙色系：类、结构体
+set_hl(0, "CmpItemKindClass", { fg = "#EBCB8B" })
+set_hl(0, "CmpItemKindStruct", { fg = "#EBCB8B" })
+set_hl(0, "CmpItemKindInterface", { fg = "#EBCB8B" })
+
+-- 红色系：片段 (Snippet)
+set_hl(0, "CmpItemKindSnippet", { fg = "#BF616A" })
 
 cmp.setup {
     snippet = {
@@ -12,57 +42,56 @@ cmp.setup {
     },
     window = {
         completion = {
-            border = "solid",
+            border = "none",
             scrollbar = true,
-            winhighlight = "FloatBorder:CmpBorder",
+            winhighlight = "Normal:CmpNormal,FloatBorder:CmpBorder,CursorLine:CmpSel,Search:None",
         },
         documentation = {
-            border = "solid",
+            border = "none",
             scrollbar = true,
-            winhighlight = "FloatBorder:CmpBorder",
+            winhighlight = "Normal:CmpDoc,FloatBorder:CmpDocBorder,CursorLine:CmpSel,Search:None",
         },
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-p>'] = cmp.mapping.select_prev_item(), -- 上一个
-        ['<C-n>'] = cmp.mapping.select_next_item(), -- 下一个
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),    -- 文档向上翻页
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),     -- 文档向下翻页
-        ['<C-Space>'] = cmp.mapping.complete(),     -- 手动触发补全
-        ['<C-e>'] = cmp.mapping.abort(),            -- 关闭补全窗口
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4), -- 文档向上翻页
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),  -- 文档向下翻页
+        ['<C-Space>'] = cmp.mapping.complete(),  -- 手动触发补全
+        ['<C-e>'] = cmp.mapping.abort(),         -- 关闭补全窗口
 
-        -- 回车确认：支持自动导入
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
-
-        -- Super Tab 模式：Tab 选择下一个，或者跳转代码片段
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
+                -- 这里也要加上 behavior = cmp.SelectBehavior.Select
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            elseif require('luasnip').expand_or_jumpable() then
+                require('luasnip').expand_or_jump()
             else
                 fallback()
             end
-        end, { 'i', 's' }),
+        end, { "i", "s" }),
 
         ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
+                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+            elseif require('luasnip').jumpable(-1) then
+                require('luasnip').jump(-1)
             else
                 fallback()
             end
-        end, { 'i', 's' }),
+        end, { "i", "s" }),
+
     }),
     sources = cmp.config.sources({
-        { name = 'nvim_lsp' }, -- 最高优先级：LSP 建议
-        { name = 'path' },     -- 路径
-        { name = "lazydev" },
+        { name = 'nvim_lsp',                priority = 1000 }, -- 最高优先级：LSP 建议
+        { name = 'nvim_lsp_signature_help', priority = 900 },
+        { name = 'path',                    priority = 700 },  -- 路径
+        { name = 'buffer',                  priority = 600 },  -- Buffer 内单词
+        { name = "lazydev",                 priority = 500 },
+        { name = 'nvim_lua',                priority = 400 },
+        { name = 'luasnip',                 priority = 300 }, -- 代码片段
     }, {
-        { name = 'buffer' },  -- Buffer 内单词
-        { name = 'luasnip' }, -- 代码片段
-        { name = 'nvim_lsp_signature_help' },
-        { name = 'nvim_lua' },
         {
             name = "dictionary",
             keyword_length = 2,
