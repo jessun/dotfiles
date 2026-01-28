aa14735b6469d869f913c773bcfbe0fc34405cdf
jessun <jessun.pro@gmail.com>
2026-01-28 20:40:22 +0800

    feat(nvim-outline): add F4 shortcut to toggle outline
    Introduces a new keybinding `<F4>` to toggle the outline window.
    Refactors keymap setting using a local `map` alias.

--- dot_config/nvim/lua/plugins/outline.lua
+++ dot_config/nvim/lua/plugins/outline.lua
@@ -2,4 +2,7 @@ require("outline").setup {
     -- Your setup opts here (leave empty to use defaults)
 }
 
-vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+local map = vim.keymap.set
+
+map("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+map("n", "<F4>", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
