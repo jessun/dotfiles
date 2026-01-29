ac928def9b5641f9b43d0b615b83df81ac7b172a
jessun <jessun.pro@gmail.com>
2026-01-29 23:44:36 +0800

    refactor(plugins): move nvim-surround config to opts
    Removes the dedicated 'nvim-surround.lua' file and configures the plugin
    directly within 'init.lua' using 'opts = {}'. This simplifies the plugin
    setup for default configuration.

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -183,9 +183,7 @@ local plugins = {
         "kylechui/nvim-surround",
         version = "*", -- Use for stability; omit to use `main` branch for the latest features
         event = "VeryLazy",
-        config = function()
-            load_plugin_config("nvim-surround.lua")
-        end
+        opts = {},
     },
     -- registers 快捷操作 ============================================================
     {
