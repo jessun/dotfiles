1656e0490e6433ae70a2a03d75a207534743a7f1
jessun <jessun.pro@gmail.com>
2026-01-30 07:42:00 +0800

    chore(blink): adjust nerd font variant

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -29,7 +29,7 @@ return {
     appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
+        nerd_font_variant = 'normal'
     },
 
     -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
