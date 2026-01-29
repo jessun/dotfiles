7908aaba1a6d4b7ca2dcdca1679aa3c7d8a46c49
jessun <jessun.pro@gmail.com>
2026-01-29 23:40:03 +0800

    refactor(noice): configure plugin via opts function

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -218,9 +218,6 @@ local plugins = {
     {
         "folke/noice.nvim",
         event = "VeryLazy",
-        opts = {
-            -- add any options here
-        },
         dependencies = {
             -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
             "MunifTanjim/nui.nvim",
@@ -229,8 +226,8 @@ local plugins = {
             --   If not available, we use `mini` as the fallback
             "rcarriga/nvim-notify",
         },
-        config = function()
-            load_plugin_config("noice.lua")
+        opts = function()
+            return require("plugins.noice")
         end
     },
 }
