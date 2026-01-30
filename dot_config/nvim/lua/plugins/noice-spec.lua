7d276cc53992ab67606e24ba24a897afaaaaa3b9
jessun <jessun.pro@gmail.com>
2026-01-30 23:21:16 +0800

    feat(nvim): add plugin noice

---
+++ dot_config/nvim/lua/plugins/noice-spec.lua
@@ -0,0 +1,43 @@
+-- lazy.nvim
+return {
+	"folke/noice.nvim",
+	cond = vim.g.enable_native_lsp,
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
+	event = "VeryLazy",
+	opts = {
+		-- add any options here
+		lsp = {
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+			},
+			progress = {
+				enabled = true,
+				-- Lsp Progress is formatted using the built-in lsp_progress format
+				-- and other options to format.
+				format = "lsp_progress",
+				format_done = "lsp_progress_done",
+				throttle = 1000 / 30, -- frequency to update lsp progress message
+
+				view = "mini",
+			},
+		},
+		-- you can enable a preset for easier configuration
+		presets = {
+			bottom_search = true, -- use a classic bottom cmdline for search
+			command_palette = true, -- position the cmdline and popupmenu together
+			long_message_to_split = true, -- long messages will be sent to a split
+			inc_rename = false,  -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = false, -- add a border to hover docs and signature help
+		},
+	},
+}
