36d18f81af2716ef6846540ddb4b3644c289f7f3
jessun <jessun.pro@gmail.com>
2026-01-30 21:15:59 +0800

    refactor

--- dot_config/nvim/lua/plugins/cmp_dictionary.lua
+++
@@ -1,8 +0,0 @@
-require("cmp_dictionary").setup({
-    paths = {
-        "/usr/share/dict/words",
-        "~/.config/nvim/dicts/personal",
-    },
-    exact_length = 4,
-    first_case_insensitive = true,
-})
--- dot_config/nvim/lua/plugins/outline.lua
+++
@@ -1,8 +0,0 @@
-require("outline").setup {
-    -- Your setup opts here (leave empty to use defaults)
-}
-
-local map = vim.keymap.set
-
-map("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
-map("n", "<F4>", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
