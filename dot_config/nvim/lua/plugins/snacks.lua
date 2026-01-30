f311783cc5c400365a689bf7e79e6e35221d0fd8
jessun <jessun.pro@gmail.com>
2026-01-30 16:26:50 +0800

    feat(snacks): enable and integrate zen, input, picker
    Configure `zen` mode for focused editing.
    Map `vim.ui.input` and `vim.ui.select` to `snacks.nvim`'s input and picker modules, enhancing UI interactions.

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,3 +1,10 @@
+local sk = require('snacks')
+
+local input_cfg = {
+    enabled = true,
+    icon = "",
+}
+
 local dim_cfg = {
     enabled = true,
     ---@type snacks.scope.Config
@@ -23,25 +30,64 @@ local dim_cfg = {
     end,
 
 }
-local sk = require('snacks')
+
+local zen_cfg = {
+    -- You can add any `Snacks.toggle` id here.
+    -- Toggle state is restored when the window is closed.
+    -- Toggle config options are NOT merged.
+    ---@type table<string, boolean>
+    toggles = {
+        dim = true,
+        git_signs = false,
+        mini_diff_signs = false,
+        -- diagnostics = false,
+        -- inlay_hints = false,
+    },
+    center = true,          -- center the window
+    show = {
+        statusline = false, -- can only be shown when using the global statusline
+        tabline = false,
+    },
+    ---@type snacks.win.Config
+    win = { style = "zen" },
+    --- Callback when the window is opened.
+    ---@param win snacks.win
+    on_open = function(win) end,
+    --- Callback when the window is closed.
+    ---@param win snacks.win
+    on_close = function(win) end,
+    --- Options for the `Snacks.zen.zoom()`
+    ---@type snacks.zen.Config
+    zoom = {
+        toggles = {},
+        center = false,
+        show = { statusline = true, tabline = true },
+        win = {
+            backdrop = false,
+            width = 0, -- full width
+        },
+    },
+}
+
+
 
 sk.setup {
     animate = { enabled = true },
     scroll = { enabled = true },
     bufdelete = { enabled = true },
     dim = dim_cfg,
+    zen = zen_cfg,
+    input = input_cfg,
+    picker = { enabled = true },
 
     -- gitbrowse = { enabled = true },
-    -- input = { enabled = true },
     -- lazygit = { enabled = true },
     -- notifier = { enabled = true },
-    -- picker = { enabled = true },
     -- quickfile = { enabled = true },
     -- rename = { enabled = true },
     -- scope = { enabled = true },
     -- terminal = { enabled = true },
     -- words = { enabled = true },
-    -- zen = { enabled = true },
     -- scratch = { enabled = true },
     -- debug = { enabled = true },
     -- image = { enabled = true },
@@ -61,3 +107,11 @@ local opts = { noremap = true, silent = true }
 map({ "n" }, "<leader>z", function()
     sk.dim()
 end, opts)
+
+vim.ui.input = function(data, on_confirm)
+    sk.input(data, on_confirm)
+end
+
+vim.ui.select = function(items, opts, on_choice)
+    sk.picker.select(items, opts, on_choice)
+end
