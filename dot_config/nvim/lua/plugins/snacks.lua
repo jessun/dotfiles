0529f60cf72605496f7a33ae543c45491a25be88
jessun <jessun.pro@gmail.com>
2026-01-30 19:40:57 +0800

    chore(snacks): remove zen mode configuration

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -29,44 +29,6 @@ local dim_cfg = {
 
 }
 
-local zen_cfg = {
-    -- You can add any `Snacks.toggle` id here.
-    -- Toggle state is restored when the window is closed.
-    -- Toggle config options are NOT merged.
-    ---@type table<string, boolean>
-    toggles = {
-        dim = true,
-        git_signs = false,
-        mini_diff_signs = false,
-        -- diagnostics = false,
-        -- inlay_hints = false,
-    },
-    center = true,          -- center the window
-    show = {
-        statusline = false, -- can only be shown when using the global statusline
-        tabline = false,
-    },
-    ---@type snacks.win.Config
-    win = { style = "zen" },
-    --- Callback when the window is opened.
-    ---@param win snacks.win
-    on_open = function(win) end,
-    --- Callback when the window is closed.
-    ---@param win snacks.win
-    on_close = function(win) end,
-    --- Options for the `Snacks.zen.zoom()`
-    ---@type snacks.zen.Config
-    zoom = {
-        toggles = {},
-        center = false,
-        show = { statusline = true, tabline = true },
-        win = {
-            backdrop = false,
-            width = 0, -- full width
-        },
-    },
-}
-
 Snacks.setup {
     animate = {},
     bufdelete = {},
@@ -82,7 +44,6 @@ Snacks.setup {
     terminal = {},
     toggle = {},
     words = {},
-    zen = zen_cfg,
 }
 
 vim.ui.input = function(data, on_confirm)
