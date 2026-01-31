756e574a885a956abc20657578dd1c07cb4cb1a3
jessun <jessun.pro@gmail.com>
2026-01-31 11:54:45 +0800

    chore(noice): remove conditional loading
    Always load the noice.nvim plugin, simplifying configuration.

--- dot_config/nvim/lua/plugins/noice-spec.lua
+++ dot_config/nvim/lua/plugins/noice-spec.lua
@@ -1,7 +1,6 @@
 -- lazy.nvim
 return {
     "folke/noice.nvim",
-    cond = vim.g.enable_native_lsp,
     dependencies = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
