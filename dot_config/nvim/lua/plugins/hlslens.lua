08371e02c9828119056373136a412855e883af27
jessun <jessun.pro@gmail.com>
2026-01-31 13:24:09 +0800

    feat(nvim.hlslens): add plugin nvim-hlslens

---
+++ dot_config/nvim/lua/plugins/hlslens.lua
@@ -0,0 +1,14 @@
+return {
+    'kevinhwang91/nvim-hlslens',
+    opts = {},
+    keys = {
+        { "/", mode = "n" },
+        { "?", mode = "n" },
+        { "n",  "<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>", mode = { "n" }, desc = "Hlslens next" },
+        { "N",  "<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>", mode = { "n" }, desc = "Hlslens prev" },
+        { "*",  "*<Cmd>lua require('hlslens').start()<CR>",                                             mode = { "n" }, desc = "Hlslens *" },
+        { "#",  "#<Cmd>lua require('hlslens').start()<CR>",                                             mode = { "n" }, desc = "Hlslens #" },
+        { "g*", "g*<Cmd>lua require('hlslens').start()<CR>",                                            mode = { "n" }, desc = "Hlslens g*" },
+        { "g#", "g#<Cmd>lua require('hlslens').start()<CR>",                                            mode = { "n" }, desc = "Hlslens g#" },
+    }
+}
