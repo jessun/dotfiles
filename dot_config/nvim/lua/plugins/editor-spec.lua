7f41027c40bb52ca68a603195d85092c53cd1211
jessun <jessun.pro@gmail.com>
2026-02-02 05:56:54 +0800

    feat(nvim): add auto-session plugin
    Integrates the `auto-session` plugin for automatic session management.
    Configures it to suppress session creation in common utility directories.

--- dot_config/nvim/lua/plugins/editor-spec.lua
+++ dot_config/nvim/lua/plugins/editor-spec.lua
@@ -20,4 +20,21 @@ return {
 			{ "<F3>", "<cmd>Outline<CR>", desc = "Outline: toggle" },
 		},
 	},
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+
+		---enables autocomplete for opts
+		---@module "auto-session"
+		---@type AutoSession.Config
+		opts = {
+			suppressed_dirs = {
+				"~/",
+				"~/workbench/",
+				"~/Downloads",
+				"/",
+			},
+			-- log_level = 'debug',
+		},
+	},
 }
