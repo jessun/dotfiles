fbf778e9cf4bb1d815127003384c43aa39867eae
jessun <jessun.pro@gmail.com>
2026-02-02 04:14:15 +0800

    refactor(lualine): restructure tabline config and set active color
    Move the `tabline.lualine_a` configuration to explicitly set the active buffer color using `nord` theme variables.

--- dot_config/nvim/lua/plugins/lualine-spec.lua
+++ dot_config/nvim/lua/plugins/lualine-spec.lua
@@ -27,43 +27,7 @@ return {
 			lualine_z = {},
 		},
 		tabline = {
-			lualine_a = {
-				{
-					"buffers",
-					show_filename_only = true, -- Shows shortened relative path when set to false.
-					hide_filename_extension = false, -- Hide filename extension when set to true.
-					show_modified_status = true, -- Shows indicator when the buffer is modified.
-
-					mode = 2, -- 0: Shows buffer name
-					-- 1: Shows buffer index
-					-- 2: Shows buffer name + buffer index
-					-- 3: Shows buffer number
-					-- 4: Shows buffer name + buffer number
-
-					max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-					-- it can also be a function that returns
-					-- the value of `max_length` dynamically.
-					filetype_names = {
-						TelescopePrompt = "Telescope",
-						dashboard = "Dashboard",
-						packer = "Packer",
-						fzf = "FZF",
-						alpha = "Alpha",
-					}, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
-
-					buffers_color = {
-						-- Same values as the general color option can be used here.
-						active = "lualine_c_normal",
-						inactive = "lualine_a_normal",
-					},
-
-					symbols = {
-						modified = "+", -- Text to show when the buffer is modified
-						-- alternate_file = "#", -- Text to show to identify the alternate file
-						-- directory = "", -- Text to show when the buffer is a directory
-					},
-				},
-			},
+			lualine_a = {},
 			lualine_b = {},
 			lualine_x = {},
 			lualine_y = {},
@@ -123,6 +87,43 @@ return {
 			{ "progress" },
 			{ "filetype" },
 		}
+		opts.tabline.lualine_a = {
+			{
+				"buffers",
+				show_filename_only = true, -- Shows shortened relative path when set to false.
+				hide_filename_extension = false, -- Hide filename extension when set to true.
+				show_modified_status = true, -- Shows indicator when the buffer is modified.
+
+				mode = 2, -- 0: Shows buffer name
+				-- 1: Shows buffer index
+				-- 2: Shows buffer name + buffer index
+				-- 3: Shows buffer number
+				-- 4: Shows buffer name + buffer number
+
+				max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+				-- it can also be a function that returns
+				-- the value of `max_length` dynamically.
+				filetype_names = {
+					TelescopePrompt = "Telescope",
+					dashboard = "Dashboard",
+					packer = "Packer",
+					fzf = "FZF",
+					alpha = "Alpha",
+				}, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
+
+				buffers_color = {
+					-- Same values as the general color option can be used here.
+					active = { bg = nord.base.bg, fg = nord.base.fg },
+					inactive = "lualine_a_normal",
+				},
+				symbols = {
+					modified = "+", -- Text to show when the buffer is modified
+					-- alternate_file = "#", -- Text to show to identify the alternate file
+					-- directory = "", -- Text to show when the buffer is a directory
+				},
+			},
+		}
+
 		require("lualine").setup(opts)
 	end,
 }
