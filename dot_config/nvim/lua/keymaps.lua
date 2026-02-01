001a85b9e452580e3f56b38852c3776e93b28c9b
jessun <jessun.pro@gmail.com>
2026-02-01 11:17:30 +0800

    feat(nvim): toggle help window with F1
    Closes an existing help window if open, otherwise opens the default help.

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -57,6 +57,31 @@ keyset("n", "<F12>", function()
 	vim.fn.setreg("+", path)
 	print("Copied path: " .. path)
 end, opts("Nvim: copy path to clipboard"))
+
+keyset({ "n", "i", "v" }, "<F1>", function()
+	-- 1. 获取当前所有窗口的列表
+	local wins = vim.api.nvim_list_wins()
+	local help_win = nil
+
+	-- 2. 遍历查找是否有窗口的 buftype 是 'help'
+	for _, win in ipairs(wins) do
+		local buf = vim.api.nvim_win_get_buf(win)
+		-- 检查 buffer 的 buftype 属性
+		if vim.api.nvim_get_option_value("buftype", { buf = buf }) == "help" then
+			help_win = win
+			break
+		end
+	end
+
+	-- 3. 判断逻辑
+	if help_win then
+		-- A. 如果找到了帮助窗口，就关闭它
+		vim.api.nvim_win_close(help_win, true)
+	else
+		-- B. 如果没找到，就执行默认的打开帮助命令
+		vim.cmd("help")
+	end
+end, { desc = "Toggle Help Window" })
 -- ============================================================================
 -- End of file
 -- ============================================================================
