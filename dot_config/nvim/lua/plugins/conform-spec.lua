6371c745ac80064257306ab41503072eff870bc7
jessun <jessun.pro@gmail.com>
2026-02-02 18:21:25 +0800

    style(conform): adjust indentation

--- dot_config/nvim/lua/plugins/conform-spec.lua
+++ dot_config/nvim/lua/plugins/conform-spec.lua
@@ -1,31 +1,31 @@
 return {
-    'stevearc/conform.nvim',
-    event = { "BufWritePre" },
-    opts = {
-        formatters_by_ft = {
-            rust = { "rustfmt", lsp_format = "fallback" },
-            lua = { "stylua", lsp_format = "fallback" },
-        },
-        format_on_save = {
-            -- These options will be passed to conform.format()
-            timeout_ms = 999999999,
-            lsp_fallback = true,
-            lsp_format = "fallback",
-        },
-    },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	opts = {
+		formatters_by_ft = {
+			rust = { "rustfmt", lsp_format = "fallback" },
+			lua = { "stylua", lsp_format = "fallback" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 999999999,
+			lsp_fallback = true,
+			lsp_format = "fallback",
+		},
+	},
 
-    config = function(_, opts)
-        require("conform").setup(opts) -- 必须调用这一行！
-        vim.api.nvim_create_user_command("Format", function(args)
-            local range = nil
-            if args.count ~= -1 then
-                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-                range = {
-                    start = { args.line1, 0 },
-                    ["end"] = { args.line2, end_line:len() },
-                }
-            end
-            require("conform").format({ async = true, lsp_fallback = true, range = range })
-        end, { range = true })
-    end
+	config = function(_, opts)
+		require("conform").setup(opts) -- 必须调用这一行！
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			require("conform").format({ async = true, lsp_fallback = true, range = range })
+		end, { range = true })
+	end,
 }
