ec42b742bf7eaeef431c0bfedfd3db06234841be
jessun <jessun.pro@gmail.com>
2026-02-04 18:34:12 +0800

    chore(edgy): disable animations

--- dot_config/nvim/lua/plugins/edgy-spec.lua
+++ dot_config/nvim/lua/plugins/edgy-spec.lua
@@ -83,14 +83,14 @@ return {
 		},
 
 		animate = {
-			enabled = true,
+			enabled = false,
 			fps = 100, -- frames per second
 			cps = 120, -- cells per second
 			on_begin = function()
 				vim.g.minianimate_disable = true
 			end,
 			on_end = function()
-				vim.g.minianimate_disable = false
+				vim.g.minianimate_disable = true
 			end,
 			-- Spinner for pinned views that are loading.
 			-- if you have noice.nvim installed, you can use any spinner from it, like:
