9fdd83d0d87d1d225589168e9f174201a41279b3
jessun <jessun.pro@gmail.com>
2026-02-07 11:01:59 +0800

    feat(luasnip): add rust tokio main snippet and config
    Configure luasnip to load snippets from `~/.config/nvim/snippets`.
    Adds a new `tokio_main` snippet for Rust to quickly create a `#[tokio::main]` async function.

--- dot_config/nvim/lua/plugins/luasnip-spec.lua
+++ dot_config/nvim/lua/plugins/luasnip-spec.lua
@@ -40,6 +40,8 @@ return {
 		},
 	},
 	config = function()
-		require("luasnip.loaders.from_vscode").lazy_load()
+		require("luasnip.loaders.from_vscode").lazy_load({
+			paths = { vim.fn.expand("~/.config/nvim/snippets") },
+		})
 	end,
 }
