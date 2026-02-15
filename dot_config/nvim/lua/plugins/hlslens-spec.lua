fc3e2c6bcc552fac443fdb99eacffa76e2246edc
jessun <jessun.pro@gmail.com>
2026-02-16 00:44:38 +0800

    style(plugins): reformat hlchunk and hlslens configs

--- dot_config/nvim/lua/plugins/hlslens-spec.lua
+++ dot_config/nvim/lua/plugins/hlslens-spec.lua
@@ -1,14 +1,44 @@
 return {
-    'kevinhwang91/nvim-hlslens',
-    opts = {},
-    keys = {
-        { "/", mode = "n" },
-        { "?", mode = "n" },
-        { "n",  "<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>", mode = { "n" }, desc = "Hlslens next" },
-        { "N",  "<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>", mode = { "n" }, desc = "Hlslens prev" },
-        { "*",  "*<Cmd>lua require('hlslens').start()<CR>",                                             mode = { "n" }, desc = "Hlslens *" },
-        { "#",  "#<Cmd>lua require('hlslens').start()<CR>",                                             mode = { "n" }, desc = "Hlslens #" },
-        { "g*", "g*<Cmd>lua require('hlslens').start()<CR>",                                            mode = { "n" }, desc = "Hlslens g*" },
-        { "g#", "g#<Cmd>lua require('hlslens').start()<CR>",                                            mode = { "n" }, desc = "Hlslens g#" },
-    }
+	"kevinhwang91/nvim-hlslens",
+	opts = {},
+	keys = {
+		{ "/", mode = "n" },
+		{ "?", mode = "n" },
+		{
+			"n",
+			"<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>",
+			mode = { "n" },
+			desc = "Hlslens next",
+		},
+		{
+			"N",
+			"<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>",
+			mode = { "n" },
+			desc = "Hlslens prev",
+		},
+		{
+			"*",
+			"*<Cmd>lua require('hlslens').start()<CR>",
+			mode = { "n" },
+			desc = "Hlslens *",
+		},
+		{
+			"#",
+			"#<Cmd>lua require('hlslens').start()<CR>",
+			mode = { "n" },
+			desc = "Hlslens #",
+		},
+		{
+			"g*",
+			"g*<Cmd>lua require('hlslens').start()<CR>",
+			mode = { "n" },
+			desc = "Hlslens g*",
+		},
+		{
+			"g#",
+			"g#<Cmd>lua require('hlslens').start()<CR>",
+			mode = { "n" },
+			desc = "Hlslens g#",
+		},
+	},
 }
